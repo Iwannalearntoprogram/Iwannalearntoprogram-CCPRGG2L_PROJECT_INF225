fa770dc1ac36f0bc292cb05d36a0092fb8039374
Mr.Shelji <120694178+KENJIMENDOZA@users.noreply.github.com>
2023-03-08 16:27:33 +0000

    Update MainMenu.java

--- RockPaperScissors/src/MainMenu.java
+++ RockPaperScissors/src/MainMenu.java
@@ -6,11 +6,11 @@ public class MainMenu extends JFrame {
     private JButton startButton;
 
     MainMenu() {
-        // Create and set up the Start Game button
+        
         startButton = new JButton("Start Game");
         startButton.setFont(new Font("Times New Roman", Font.BOLD, 25));
         startButton.addActionListener(e -> {
-            // Open the ColorMenu and set the background color based on the user's selection
+            
             ColorMenu colorMenu = new ColorMenu();
             colorMenu.setVisible(true);
             colorMenu.colorList.addListSelectionListener(event -> {
@@ -21,7 +21,7 @@ public class MainMenu extends JFrame {
         });
         this.add(startButton);
 
-        // Set up the JFrame
+        
         this.setLayout(new FlowLayout());
         this.setTitle("Rock Paper Scissors");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
