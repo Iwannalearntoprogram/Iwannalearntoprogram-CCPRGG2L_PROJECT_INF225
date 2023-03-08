7064d85435d7fe87758d84479868e2968dc96582
Mr.Shelji <120694178+KENJIMENDOZA@users.noreply.github.com>
2023-03-08 12:01:47 +0000

    Update ColorMenu.java

--- RockPaperScissors/src/ColorMenu.java
+++ RockPaperScissors/src/ColorMenu.java
@@ -9,48 +9,41 @@ import java.awt.*;
 
 public class ColorMenu extends JFrame{
 
-    // Color name array
+    
     private String[] colorNameArray = {"GRAY", "BLACK", "WHITE",};
-    // Color list array
+    
     private Color[] colorListArray = {Color.GRAY, Color.BLACK, Color.WHITE};
-    // JList
+    
     JList colorList;
 
     ColorMenu(){
 
-        // JLabel
+        
         JLabel label = new JLabel();
         label.setText("Choose your color: ");
         label.setFont(new Font("Times New Roman", Font.BOLD, 25));
         this.add(label);
 
-        // JList
+       
         colorList = new JList(colorNameArray);
         colorList.setFont(new Font("Times New Roman", Font.BOLD, 25));
-        // Allows only one selection in the list
+        
         colorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        // Add event
+      
         EventHandler handler = new EventHandler();
         colorList.addListSelectionListener(handler);
 
-        // Add list to frame
+       
         this.add(colorList);
 
-        // Frame Layout
-        this.setLayout(new FlowLayout());
-        // Frame title
-        this.setTitle("Color Menu");
-        // Terminates java program on close
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // Frame size
-        this.setSize(300, 300);
-        // Puts frame in the middle
-        this.setLocationRelativeTo(null);
-        // Make frame appear
-        this.setVisible(true);
-        // Disable resize
-        this.setResizable(false);
-        // Set background color
+        
+        this.setLayout(new FlowLayout());        
+        this.setTitle("Color Menu");        
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);       
+        this.setSize(300, 300);        
+        this.setLocationRelativeTo(null);       
+        this.setVisible(true);        
+        this.setResizable(false);        
         this.getContentPane().setBackground(Color.CYAN);
     }
     
