80b3700bb1442c92b09b690ec6cb3b409df60615
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2023-03-11 23:26:05 +0800

    connected

--- RockPaperScissors/src/MainMenu.java
+++ RockPaperScissors/src/MainMenu.java
@@ -1,38 +1,93 @@
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.xml.stream.events.StartDocument;
+
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
+import java.awt.event.ActionEvent;
 
-public class MainMenu extends JFrame {
 
-    private JButton startButton;
 
-    MainMenu() {
-        
-        startButton = new JButton("Start Game");
-        startButton.setFont(new Font("Times New Roman", Font.BOLD, 25));
-        startButton.addActionListener(e -> {
-            
-            ColorMenu colorMenu = new ColorMenu();
-            colorMenu.setVisible(true);
-            colorMenu.colorList.addListSelectionListener(event -> {
-            Color selectedColor = colorMenu.colorListArray[colorMenu.colorList.getSelectedIndex()];
-            getContentPane().setBackground(selectedColor);
-                
-            });
-        });
-        this.add(startButton);
 
-        
-        this.setLayout(new FlowLayout());
-        this.setTitle("Rock Paper Scissors");
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(300, 300);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-        this.setResizable(false);
-        this.getContentPane().setBackground(Color.WHITE);
-    }
+public class MainMenu extends JFrame{
 
-    public static void main(String[] args) {
-        new MainMenu();
-    }
+    MainMenu(){
+
+
+    JLabel Logo = new JLabel();
+    JLabel Logoname= new JLabel();
+    
+
+    Logoname.setText("ROCKPAPERSCISSORS GAME!!");
+    Logoname.setHorizontalTextPosition(JLabel.CENTER);
+    Logoname.setVerticalTextPosition(JLabel.TOP);
+    Logoname.setVerticalAlignment(JLabel.TOP);
+    Logoname.setHorizontalAlignment(JLabel.CENTER);
+    Logoname.setFont(new Font("ARIAL", Font.BOLD, 50));
+    Logoname.setLocation(800,800);
+
+
+    
+    JButton startButton= new JButton("Start Game");
+
+    startButton.setText("Start Game");
+    startButton.setFont(new Font("Arial", Font.BOLD, 25));
+    startButton.setSize(100,100);
+    startButton.setBounds(100, 100, 250, 250);
+    startButton.setLocation(800, 380);
+    startButton.setVerticalTextPosition(JButton.CENTER);
+    startButton.setHorizontalTextPosition(JButton.CENTER);
+    startButton.setLayout(new FlowLayout());
+    startButton.setFocusable(false);
+
+    EventHandler startHandler= new EventHandler();
+
+    startButton.addActionListener(startHandler);
+
+
+    this.add(startButton);
+    this.add(Logoname);
+
+    this.setTitle("RockPaperScissors");
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setSize(800,800);
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+    this.setResizable(true);
+    this.getContentPane().setBackground(Color.magenta);
+
+
+    
 }
+
+    private class EventHandler implements ActionListener{
+
+
+     public void actionPerformed(ActionEvent event){
+
+        new ColorMenu();
+
+        dispose();
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
+
