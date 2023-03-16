bb35ae48490b320fcef4698b206fe1671e999032
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2023-03-16 20:51:20 +0800

    added score

--- RockPaperScissors/src/HRPS.java
+++ RockPaperScissors/src/HRPS.java
@@ -14,7 +14,10 @@ public class HRPS extends JFrame implements ActionListener {
     private JLabel resultLabel;
     private JButton Yes;
     private JButton No;
+    private JLabel playerScoreD;
+    private int playerScore;
     Container c = getContentPane();
+    
 
     
 
@@ -95,11 +98,19 @@ public class HRPS extends JFrame implements ActionListener {
 
         // Create the result label
         resultLabel = new JLabel();
+        playerScoreD = new JLabel("Score: " + playerScore);
+        playerScoreD.setFont(new Font("Arial", Font.BOLD, 15));
+        playerScoreD.setVerticalTextPosition(JLabel.BOTTOM);
+        playerScoreD.setHorizontalTextPosition(JLabel.RIGHT);
+        playerScoreD.setHorizontalAlignment(JLabel.RIGHT);
+        playerScoreD.setVerticalTextPosition(JLabel.BOTTOM);
+        playerScoreD.setLocation(500,500);
 
        // Add the buttons and result label to the content pane
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
         c.setSize(250, 1500);
+        c.add(playerScoreD);
         c.add(rockButton);
         c.add(paperButton);
         c.add(scissorsButton);
@@ -156,6 +167,9 @@ public class HRPS extends JFrame implements ActionListener {
                 || (playerChoice.equals("spock") && computerChoice.equals("rock"))
                 || (playerChoice.equals("lizard") && computerChoice.equals("spock"))
                 || (playerChoice.equals("lizard") && computerChoice.equals("paper"))) {
+
+                    playerScore++;
+                    playerScoreD.setText("Score: "+ playerScore);
             result = "You win!";
         } else {
             result = "Computer wins!";
